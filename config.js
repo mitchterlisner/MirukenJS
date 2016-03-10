4ef2a2b90e643d591b879db317452204348adb5c
Michael Dudley <michael.dudley@improvingenterprises.com>
2016-03-10 08:11:41 -0600

    Configuring the jspm dependencies

--- config.js
+++ config.js
@@ -13,10 +13,23 @@ System.config({
     "npm:*": "jspm_packages/npm/*"
   },
 
+  packages: {
+    "src/ioc": {
+      "main": "index.js",
+      "format": "cjs"
+    },
+    "src/validate": {
+      "main": "index.js",
+      "format": "cjs"
+    }
+  },
+
   map: {
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
+    "bluebird": "npm:bluebird@3.3.4",
     "core-js": "npm:core-js@1.2.6",
+    "validate.js": "npm:validate.js@0.9.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -35,6 +48,14 @@ System.config({
     "npm:babel-runtime@5.8.35": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
+    "npm:bluebird@3.3.4": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:chart.js@1.0.2": {
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
@@ -52,6 +73,9 @@ System.config({
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:validate.js@0.9.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
